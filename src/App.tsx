dbcb65b557e9a767ae08e09778460759916ea316
rizqyhbb <rizqyhbb@gmail.com>
2024-06-06 06:44:27 +0700

    animate svg

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,19 @@ const gridContainerVariants = {
   },
 };
 
+const svgVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgb(252, 211, 77, 0)",
+  },
+  show: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgb(252, 211, 77, 1)",
+  },
+};
+
 const gridChildVariants = { hidden: { opacity: 0 }, show: { opacity: 1 } };
 function App() {
   const { scrollYProgress: scrollCompletion } = useScroll();
@@ -106,10 +119,41 @@ function App() {
           />
         </motion.div>
       </motion.div>
+
+      {/* SVG animation */}
       <motion.div
         variants={gridChildVariants}
         className="aspect-square bg-slate-800 border-none flex justify-center items-center rounded-lg"
-      />
+      >
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="w-1/2 stroke-amber-500 stroke-[0.5]"
+        >
+          <motion.path
+            d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+            variants={svgVariants}
+            initial="hidden"
+            animate="show"
+            transition={{
+              default: {
+                duration: 2,
+                ease: "easeInOut",
+                delay: 1,
+                repeat: Infinity,
+                repeatType: "reverse",
+                repeatDelay: 1,
+              },
+              fill: {
+                duration: 2,
+                ease: "easeInOut",
+                delay: 2,
+                repeat: Infinity,
+              },
+            }}
+          />
+        </motion.svg>
+      </motion.div>
     </motion.div>
   );
 }
